734614abdbdd23ea058f312a836e970868b08517
Isaiah <44090193+EnumInt@users.noreply.github.com>
2022-03-08 12:17:06 -0500

    clean up code

--- isaiah-c-stuff/random-numbers/main.c
+++ isaiah-c-stuff/random-numbers/main.c
@@ -5,19 +5,25 @@
 int randomnum;
 int seed;
 
-int main(void) {
-
+int mkseed(void) {
   time_t seconds;
   seconds = time(NULL);
 
   seed = 1024 * seconds;
   randomnum = seed * seconds;
-  
+  // i don't even know if i have to return this function :p
+  return 0;
+}
+
+int main(void) {
+
   //print intro text
   printf("Welcome to the Random Number Generator!\n");
   printf("=======================================\n");
+  mkseed();
+  
   printf("\e[0;92m");
   printf("Your random number is: %i\n", randomnum);
-  
+
   return 0;
 }
